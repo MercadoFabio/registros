5bb59ea8941b16379ec23d31362c994c370886d1
fabio mercado <fabiomercadon@gmail.com>
2022-04-15 22:12:03 -0300

    subimos paagina web de registros

---
+++ js/index.js
@@ -0,0 +1,31 @@
+function dirigirme(id) {
+  
+     //Iconos redes sociales curriculum
+     if (id == 2) {
+      window.open(
+        "https://drive.google.com/file/d/1MRIhp8B3e-AMK0tk-Zf2vrLDd0D2hBfX/view?usp=sharing",
+        "_blank"
+      );
+    }
+    //Iconos redes sociales instagram
+    if (id == 3) {
+      window.open(
+        "https://www.instagram.com/fabiomercadon/",
+        "_blank"
+      );
+    }
+    //Iconos redes sociales linkedin
+    if (id == 4) {
+      window.open(
+        "https://www.linkedin.com/in/fabio-mercado-26a64a209/",
+        "_blank"
+      );
+    }
+    //Iconos redes sociales Git Hub
+    if (id == 5) {
+      window.open(
+        "https://github.com/fabiomercadon",
+        "_blank"
+      );
+    }
+  }
